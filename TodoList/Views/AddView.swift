c23120a70d3397ce534b7b2a3951382b05324c48
Oncu <oohancan@gmail.com>
2022-03-09 16:35:38 +0300

    + Alert Lesson #3 - 25.55

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -12,6 +12,9 @@ struct AddView: View {
     @Environment(\.presentationMode) var presentationMode
     @EnvironmentObject var listViewModel: ListViewModel
     @State var textfield: String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     var body: some View {
         ScrollView {
             VStack {
@@ -34,10 +37,24 @@ struct AddView: View {
             .padding(16)
         }
             .navigationTitle("Add an Item 🖊")
+            .alert(isPresented: $showAlert, content: getAlert)
     }
     func saveButtonPressed() {
+        if textIsAppropriate() {
         listViewModel.addItem(title: textfield)
         presentationMode.wrappedValue.dismiss()
+    }
+    }
+    func textIsAppropriate() -> Bool {
+        if textfield.count < 3 {
+            alertTitle = "Your new item must be at least 3 characters"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 struct AddView_Previews: PreviewProvider {
