a63083590da49dadb01b2c7d89c585cb6c753d87
Oncu <oohancan@gmail.com>
2022-03-20 01:55:58 +0300

    add func saveItems, finish lesson #4

--- TodoList/ViewModels/ListViewModel.swift
+++ TodoList/ViewModels/ListViewModel.swift
@@ -8,20 +8,24 @@
 import Foundation
 
 class ListViewModel: ObservableObject {
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     func getItems() {
-        
-        let newItems = [
-            ItemModel(title: "First item", isCompleted: false),
-            ItemModel(title: "Second item", isCompleted: true),
-            ItemModel(title: "Third", isCompleted: false),
-            ]
-        items.append(contentsOf: newItems)
-        
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        self.items = savedItems
     }
     func deleteItem(indexSet: IndexSet) {
         items.remove(atOffsets: indexSet)
@@ -37,6 +41,11 @@ class ListViewModel: ObservableObject {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
